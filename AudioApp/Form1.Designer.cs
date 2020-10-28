631731f875029de58aa92f9cb03d2c358ebd73e4
gruszkamateusz <mati.gruszka99@gmail.com>
2020-10-28 02:14:38 +0100

    Nagrywanie oraz zapis do pliku wav

--- AudioApp/Form1.Designer.cs
+++ AudioApp/Form1.Designer.cs
@@ -28,29 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.openBtn = new System.Windows.Forms.Button();
             this.pauseBtn = new System.Windows.Forms.Button();
             this.buttonWMP = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.sourceList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.waveBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.refBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // openBtn
             // 
-            this.button1.Location = new System.Drawing.Point(27, 10);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(284, 37);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Open Wav File";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.openBtn.Location = new System.Drawing.Point(36, 12);
+            this.openBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(157, 44);
+            this.openBtn.TabIndex = 0;
+            this.openBtn.Text = "Open Wav File";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
             // 
             // pauseBtn
             // 
             this.pauseBtn.Enabled = false;
-            this.pauseBtn.Location = new System.Drawing.Point(27, 67);
-            this.pauseBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.pauseBtn.Location = new System.Drawing.Point(36, 82);
+            this.pauseBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pauseBtn.Name = "pauseBtn";
-            this.pauseBtn.Size = new System.Drawing.Size(284, 37);
+            this.pauseBtn.Size = new System.Drawing.Size(157, 40);
             this.pauseBtn.TabIndex = 1;
             this.pauseBtn.Text = "Pause/Play wav";
             this.pauseBtn.UseVisualStyleBackColor = true;
@@ -58,35 +69,122 @@
             // 
             // buttonWMP
             // 
-            this.buttonWMP.Location = new System.Drawing.Point(59, 204);
+            this.buttonWMP.Location = new System.Drawing.Point(225, 13);
+            this.buttonWMP.Margin = new System.Windows.Forms.Padding(4);
             this.buttonWMP.Name = "buttonWMP";
-            this.buttonWMP.Size = new System.Drawing.Size(141, 46);
+            this.buttonWMP.Size = new System.Drawing.Size(181, 43);
             this.buttonWMP.TabIndex = 2;
             this.buttonWMP.Text = "Windows Media Player";
             this.buttonWMP.UseVisualStyleBackColor = true;
             this.buttonWMP.Click += new System.EventHandler(this.buttonWMP_Click);
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(36, 137);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.sourceList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.waveBtn);
+            this.splitContainer1.Panel2.Controls.Add(this.stopBtn);
+            this.splitContainer1.Panel2.Controls.Add(this.refBtn);
+            this.splitContainer1.Size = new System.Drawing.Size(414, 286);
+            this.splitContainer1.SplitterDistance = 241;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // sourceList
+            // 
+            this.sourceList.BackgroundImageTiled = true;
+            this.sourceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.sourceList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sourceList.HideSelection = false;
+            this.sourceList.Location = new System.Drawing.Point(0, 0);
+            this.sourceList.MultiSelect = false;
+            this.sourceList.Name = "sourceList";
+            this.sourceList.Size = new System.Drawing.Size(241, 286);
+            this.sourceList.TabIndex = 0;
+            this.sourceList.UseCompatibleStateImageBehavior = false;
+            this.sourceList.View = System.Windows.Forms.View.Details;
+            this.sourceList.SelectedIndexChanged += new System.EventHandler(this.sourceList_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Device";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Channels";
+            this.columnHeader2.Width = 80;
+            // 
+            // waveBtn
+            // 
+            this.waveBtn.Location = new System.Drawing.Point(17, 131);
+            this.waveBtn.Name = "waveBtn";
+            this.waveBtn.Size = new System.Drawing.Size(135, 23);
+            this.waveBtn.TabIndex = 5;
+            this.waveBtn.Text = "To wave";
+            this.waveBtn.UseVisualStyleBackColor = true;
+            this.waveBtn.Click += new System.EventHandler(this.waveBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(16, 160);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(136, 27);
+            this.stopBtn.TabIndex = 2;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            // 
+            // refBtn
+            // 
+            this.refBtn.Location = new System.Drawing.Point(17, 13);
+            this.refBtn.Name = "refBtn";
+            this.refBtn.Size = new System.Drawing.Size(136, 25);
+            this.refBtn.TabIndex = 0;
+            this.refBtn.Text = "Refresh sources";
+            this.refBtn.UseVisualStyleBackColor = true;
+            this.refBtn.Click += new System.EventHandler(this.refBtn_Click);
+            // 
             // AudioApp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.buttonWMP);
             this.Controls.Add(this.pauseBtn);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.openBtn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AudioApp";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AudioApp_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button openBtn;
         private System.Windows.Forms.Button pauseBtn;
         private System.Windows.Forms.Button buttonWMP;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView sourceList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button refBtn;
+        private System.Windows.Forms.Button waveBtn;
     }
 }
 
--- AudioApp/Form1.cs
+++ AudioApp/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using NAudio.Wave;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 
@@ -16,7 +17,9 @@ namespace AudioApp
         private NAudio.Wave.DirectSoundOut output = null;
         private WMPLib.WindowsMediaPlayer Player;
 
-        private void button1_Click(object sender, EventArgs e)
+
+        //otwieranie pliku wav
+        private void openBtn_Click(object sender, EventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
             open.Filter = " Wave File (*.wav)|*.wav;";
@@ -31,7 +34,7 @@ namespace AudioApp
 
             pauseBtn.Enabled = true;
         }
-
+        
         private void pauseBtn_Click(object sender, EventArgs e)
         {
             if(output != null)
@@ -54,6 +57,23 @@ namespace AudioApp
             {
                 wave.Dispose();
                 wave = null;
+            }
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+            if (sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+            if (waveWriter != null)
+            {
+                waveWriter.Dispose();
+                waveWriter = null;
             }
         }
 
@@ -93,6 +113,85 @@ namespace AudioApp
             open.Filter = " MP3 File (*.mp3)|*.mp3;";
             if (open.ShowDialog() != DialogResult.OK) return;
             PlayFile(open.FileName); //odtwarza plik .mp3 z podanego adresu URL
+        }
+
+        private void sourceList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void refBtn_Click(object sender, EventArgs e)
+        {
+            // tworzymy liste mozliwych zrodel dzwieku,
+            List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+            for(int i=0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i)); // dodajemy zrodla do listy sources
+            }
+            sourceList.Items.Clear(); // upewniamy sie ze lista sourceList w oknie jest pusta
+            foreach(var source in sources)
+            {
+                ListViewItem item = new ListViewItem(source.ProductName);   // tworzymy obiekt listy w oknie
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                sourceList.Items.Add(item); // dodajemy do listy w oknie
+            }
+
+        }
+        private NAudio.Wave.WaveIn sourceStream = null;    // wejscie 
+        private NAudio.Wave.DirectSoundOut waveOut = null; // wyjscie
+        private NAudio.Wave.WaveFileWriter waveWriter = null; // zapisywanie pliku
+
+        // wywolanie dispose oraz zatrzymanie i ustawienie na null wszystkiego co uzywamy
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            if( waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+            if (sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+            if(waveWriter != null)
+            {
+                waveWriter.Dispose();
+                waveWriter = null;
+            }
+        }
+        // WaveInEventArgs - jesli zmienia się pobierany dzwiek to
+        private void sourceStream_DataAvailable(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+
+            waveWriter.WriteData(e.Buffer, 0, e.BytesRecorded); //buforujemy dane
+            waveWriter.Flush(); // czyszczenie buforu dla tego strumienia
+
+        }
+
+        private void waveBtn_Click(object sender, EventArgs e)
+        {
+            if (sourceList.SelectedItems.Count == 0) return;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Wave File (*.wav)|*.wav;";
+            // jesli cos sie nie zgadza sie w oknie dialogowym - return
+            if (save.ShowDialog() != System.Windows.Forms.DialogResult.OK) return; 
+
+            int deviceNumber = sourceList.SelectedItems[0].Index;
+            sourceStream = new NAudio.Wave.WaveIn(); // przechowuje to co wchodzi 
+            sourceStream.DeviceNumber = deviceNumber; // z danego urzadzenia
+            // format strumienia
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+            // kiedy jest mozliwe pobranie nowych danych - pobierz ze strumienia
+            sourceStream.DataAvailable += new EventHandler<WaveInEventArgs>(sourceStream_DataAvailable);
+            // zapisz plik o takiej nazwie w formacie wav
+            waveWriter = new NAudio.Wave.WaveFileWriter(save.FileName, sourceStream.WaveFormat);
+
+            sourceStream.StartRecording();
         }
     }
 }
